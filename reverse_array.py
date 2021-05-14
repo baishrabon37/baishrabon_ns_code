f63e1cf7bc75d4d55ea040b95fab1c141ab1e0b0
baishrabon37 <84203287+baishrabon37@users.noreply.github.com>
2021-05-15 01:25:28 +0530

    Create reverse_array.py

---
+++ reverse_array.py
@@ -0,0 +1,23 @@
+  
+# -*- coding: utf-8 -*-
+"""
+@author: Baishrabon Karar
+"""
+
+T=int(input())
+ns=[]
+arr1=[]
+
+for i in range(T):
+    n=input().split(" ")
+    ns.append(n)
+    arr1.append(input().split(" "))
+    
+for i in range(T):
+    n=ns[i]
+    arr=arr1[i]
+    for i in range(len(arr)):
+        arr[i]=int(arr[i])
+        
+    arr.reverse()
+    print(arr)
